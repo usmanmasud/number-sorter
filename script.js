970c83891ede5251f7e6404432a1784d3945aa54
usmanmasud <168911969+usmanmasud@users.noreply.github.com>
2024-07-21 11:02:18 +0100

    commented unused function

--- script.js
+++ script.js
@@ -21,50 +21,50 @@ const updateUI = (array = []) => {
   })
 }
 
-const bubbleSort = (array) => {
-  for (let i = 0; i < array.length; i++) {
-    for (let j = 0; j < array.length - 1; j++) {
-      if (array[j] > array[j + 1]) {
-        const temp = array[j];
-        array[j] = array[j + 1];
-        array[j + 1] = temp;
-      }
-    }
-  }
+// const bubbleSort = (array) => {
+//   for (let i = 0; i < array.length; i++) {
+//     for (let j = 0; j < array.length - 1; j++) {
+//       if (array[j] > array[j + 1]) {
+//         const temp = array[j];
+//         array[j] = array[j + 1];
+//         array[j + 1] = temp;
+//       }
+//     }
+//   }
 
-  return array;
-}
+//   return array;
+// }
 
-const selectionSort = (array) => {
-  for (let i = 0; i < array.length; i++) {
-    let minIndex = i;
+// const selectionSort = (array) => {
+//   for (let i = 0; i < array.length; i++) {
+//     let minIndex = i;
 
-    for (let j = i + 1; j < array.length; j++) {
-      if (array[j] < array[minIndex]) {
-        minIndex = j;
-      }
-    }
+//     for (let j = i + 1; j < array.length; j++) {
+//       if (array[j] < array[minIndex]) {
+//         minIndex = j;
+//       }
+//     }
 
-    const temp = array[i];
-    array[i] = array[minIndex];
-    array[minIndex] = temp;
-  }
+//     const temp = array[i];
+//     array[i] = array[minIndex];
+//     array[minIndex] = temp;
+//   }
 
-  return array;
-}
+//   return array;
+// }
 
-const insertionSort = (array) => {
-  for (let i = 1; i < array.length; i++) {
-    const currValue = array[i];
-    let j = i - 1;
+// const insertionSort = (array) => {
+//   for (let i = 1; i < array.length; i++) {
+//     const currValue = array[i];
+//     let j = i - 1;
 
-    while (j >= 0 && array[j] > currValue) {
-      array[j + 1] = array[j];
-      j--;
-    }
-    array[j + 1] = currValue;
-  }
-  return array;
-}
+//     while (j >= 0 && array[j] > currValue) {
+//       array[j + 1] = array[j];
+//       j--;
+//     }
+//     array[j + 1] = currValue;
+//   }
+//   return array;
+// }
 
 sortButton.addEventListener("click", sortInputArray);
